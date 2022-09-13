9355dcf5aef5e537f88f02f81cdec1923b7d2537
smoyamedina <smoyamedina@outlook.com>
2022-09-13 00:57:01 -0400

    close nav on click event for /# hrefs

--- src/script.js
+++ src/script.js
@@ -3,6 +3,7 @@
 const menuToggle = document.getElementById("toggle-nav-id");
 const bodyElement = document.getElementById("body");
 
+// ***** freeze body content behind nav so it doesn't scroll while interacting with links ***** //
 let bodyNoScroll = () => {
 	if (menuToggle.checked) {
 		bodyElement.classList.add("no-scroll");
@@ -10,5 +11,16 @@ let bodyNoScroll = () => {
 		bodyElement.classList.remove("no-scroll");
 	}
 };
-
 menuToggle.addEventListener("click", bodyNoScroll);
+
+// ***** trying to close nav when ID links are clicked ***** //
+
+let nodeLinks = document.querySelectorAll('a[href*="/#"]');
+
+let links = Array.from(nodeLinks);
+
+links.forEach(function (item) {
+	item.addEventListener("click", () => {
+		menuToggle.click();
+	});
+});
