ee12138aa4f59123433b61877e028901ac208a7d
smoyamedina <smoyamedina@outlook.com>
2022-11-21 12:59:50 -0500

    Exp with Layout R2

--- src/script.js
+++ src/script.js
@@ -17,16 +17,16 @@ gsap.registerPlugin(
 gsap.from(".container", { ease: "linear", autoAlpha: 0 });
 
 // create the scrollSmoother before your scrollTriggers
-let smoother = ScrollSmoother.create({
-	wrapper: ".wrapper",
-	content: ".content",
-	// onStop: ScrollTrigger.refresh(),
-	normalizeScroll: true,
-	smooth: 1,
-	smoothTouch: false,
-	ignoreMobileResize: true, // how long (in seconds) it takes to "catch up" to the native scroll position
-	// effects: true, // looks for data-speed and data-lag attributes on elements
-});
+// let smoother = ScrollSmoother.create({
+// 	wrapper: ".wrapper",
+// 	content: ".content",
+// 	// onStop: ScrollTrigger.refresh(),
+// 	normalizeScroll: true,
+// 	smooth: 1,
+// 	smoothTouch: false,
+// 	ignoreMobileResize: true, // how long (in seconds) it takes to "catch up" to the native scroll position
+// 	// effects: true, // looks for data-speed and data-lag attributes on elements
+// });
 // ***** CHECKBOX ***** //
 
 const menuToggle = document.getElementById("toggle-nav-id");
@@ -129,126 +129,127 @@ if (windowQuery) {
 
 // Proj Gallery, horizontal scroll
 
-let projCardAmnt = document.querySelectorAll(".project-card");
+// let projCardAmnt = document.querySelectorAll(".project-card");
 
 // let sections = gsap.utils.toArray(".project-card");
 // let gallDist = -100 * (projCardAmnt.length - 1);
-gsap.to(".project-gallery", {
-	// xPercent: `${gallDist}`,
-	xPercent: -1 * (100 - 100 / projCardAmnt.length),
-	scrollTrigger: {
-		// anticipatePin: 1,
-		trigger: ".project-gallery",
-		top: "top top",
-		pin: true,
-		ease: "none",
-		scrub: 0,
-		invalidateOnRefresh: true,
-	},
-});
+// gsap.to(".project-gallery", {
+// 	// xPercent: `${gallDist}`,
+// 	xPercent: -1 * (100 - 100 / projCardAmnt.length),
+// 	scrollTrigger: {
+// 		// anticipatePin: 1,
+// 		trigger: ".project-gallery",
+// 		start: "top top",
+// 		end: "+=1500",
+// 		pin: true,
+// 		ease: "none",
+// 		scrub: 0.3,
+// 		invalidateOnRefresh: true,
+// 	},
+// });
 
 // *** compensating for the clicks scrollSmoother prevents since it's transformed , with an animation on click *** //
-let currentURL = window.location.pathname;
-let smoothAnchorLink = (selector) => {
-	gsap.to(smoother, {
-		scrollTop: smoother.offset(selector, "top top"),
-		duration: 1,
-		ease: "power3.out",
-	});
-};
-let whenFromSubd = (anchorLink, selector) => {
-	if (window.location.href.indexOf(anchorLink) > -1) {
-		smoothAnchorLink(selector);
-	}
-};
-const findsLink = document.querySelector("[href='/#finds']");
-const workLink = document.querySelector("[href='/#work']");
+// let currentURL = window.location.pathname;
+// let smoothAnchorLink = (selector) => {
+// 	gsap.to(smoother, {
+// 		scrollTop: smoother.offset(selector, "top top"),
+// 		duration: 1,
+// 		ease: "power3.out",
+// 	});
+// };
+// let whenFromSubd = (anchorLink, selector) => {
+// 	if (window.location.href.indexOf(anchorLink) > -1) {
+// 		smoothAnchorLink(selector);
+// 	}
+// };
+// const findsLink = document.querySelector("[href='/#finds']");
+// const workLink = document.querySelector("[href='/#work']");
 
-let workNav = () => {
-	workLink.addEventListener("click", (e) => {
-		if (currentURL == "/") {
-			e.preventDefault();
-		}
-		smoothAnchorLink(".work");
-	});
-};
+// let workNav = () => {
+// 	workLink.addEventListener("click", (e) => {
+// 		if (currentURL == "/") {
+// 			e.preventDefault();
+// 		}
+// 		smoothAnchorLink(".work");
+// 	});
+// };
 
-let findNav = () => {
-	findsLink.addEventListener("click", (e) => {
-		if (currentURL == "/") {
-			e.preventDefault();
-		}
-		smoothAnchorLink(".finds");
-	});
-};
+// let findNav = () => {
+// 	findsLink.addEventListener("click", (e) => {
+// 		if (currentURL == "/") {
+// 			e.preventDefault();
+// 		}
+// 		smoothAnchorLink(".finds");
+// 	});
+// };
 
-whenFromSubd("/#work", ".work");
-whenFromSubd("/#finds", ".finds");
-workNav();
-findNav();
+// whenFromSubd("/#work", ".work");
+// whenFromSubd("/#finds", ".finds");
+// workNav();
+// findNav();
 
-// Logo
-if (document.querySelector(".hero-home")) {
-	let logotypeSelector = ".logotype a";
+// // Logo
+// if (document.querySelector(".hero-home")) {
+// 	let logotypeSelector = ".logotype a";
 
-	gsap.to(logotypeSelector, {
-		"--foreground-color": `var(--background-color)`,
-		scrollTrigger: {
-			// markers: true,
-			trigger: ".work",
-			start: "top 5%",
-			// this is such a hacky way of making this stop overlapping the pin from earlier. i'm pretty sure I need to refresh the triggers so it includes  the pinspacers or something. idk man.
-			end: "169.25% 10%",
-			// end: "bottom 5%",
-			toggleActions: "play reset play reset",
-			invalidateOnRefresh: true,
-		},
-		duration: 0.5,
-	});
+// 	gsap.to(logotypeSelector, {
+// 		"--foreground-color": `var(--background-color)`,
+// 		scrollTrigger: {
+// 			// markers: true,
+// 			trigger: ".work",
+// 			start: "top 5%",
+// 			// this is such a hacky way of making this stop overlapping the pin from earlier. i'm pretty sure I need to refresh the triggers so it includes  the pinspacers or something. idk man.
+// 			end: "169.25% 10%",
+// 			// end: "bottom 5%",
+// 			toggleActions: "play reset play reset",
+// 			invalidateOnRefresh: true,
+// 		},
+// 		duration: 0.5,
+// 	});
 
-	// Nav Color Switch only if on desktop
-	let mm = gsap.matchMedia();
-	mm.add("(min-width:980px)", () => {
-		let navItemSelector = ".nav-item a";
+// 	// Nav Color Switch only if on desktop
+// 	let mm = gsap.matchMedia();
+// 	mm.add("(min-width:980px)", () => {
+// 		let navItemSelector = ".nav-item a";
 
-		gsap.to(navItemSelector, {
-			"--foreground-color": `var(--background-color)`,
-			scrollTrigger: {
-				trigger: ".work",
-				start: "top 10%",
-				end: "169.25% 10%",
-				// end: "bottom 9%",
-				toggleActions: "play reset play reset",
-				invalidateOnRefresh: true,
-			},
-			duration: 0.5,
-		});
-	});
-}
+// 		gsap.to(navItemSelector, {
+// 			"--foreground-color": `var(--background-color)`,
+// 			scrollTrigger: {
+// 				trigger: ".work",
+// 				start: "top 10%",
+// 				end: "169.25% 10%",
+// 				// end: "bottom 9%",
+// 				toggleActions: "play reset play reset",
+// 				invalidateOnRefresh: true,
+// 			},
+// 			duration: 0.5,
+// 		});
+// 	});
+// }
 
 // For Home-page Hero Text
 
-ScrollTrigger.create({
-	trigger: ".hero-home",
-	start: "top top",
-	pin: true,
-	pinSpacing: false,
-});
+// ScrollTrigger.create({
+// 	trigger: ".hero-home",
+// 	start: "top top",
+// 	pin: true,
+// 	pinSpacing: false,
+// });
 
-gsap.to(".hero-text", {
-	y: "16%",
-	ease: "linear",
-	scale: ".98",
-	opacity: 0,
-	scrollTrigger: {
-		trigger: ".work",
-		scrub: 0.5,
-		start: "top 87%",
-		end: "top 50%",
-		toggleActions: "play pause pause reverse",
-		invalidateOnRefresh: true,
-	},
-});
+// gsap.to(".hero-text", {
+// 	y: "16%",
+// 	ease: "linear",
+// 	scale: ".98",
+// 	opacity: 0,
+// 	scrollTrigger: {
+// 		trigger: ".work",
+// 		scrub: 0.5,
+// 		start: "top 87%",
+// 		end: "top 50%",
+// 		toggleActions: "play pause pause reverse",
+// 		invalidateOnRefresh: true,
+// 	},
+// });
 
 // window.addEventListener("resize", () => {
 // 	ScrollTrigger.refresh();
